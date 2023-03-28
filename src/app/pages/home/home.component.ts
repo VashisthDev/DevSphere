3aee6f39c7514168346c4ce747c1c85bdd104de2
VashisthDev <107829512+VashisthDev@users.noreply.github.com>
2023-03-28 11:52:17 +0530

    *

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
 
+  constructor(private elementRef: ElementRef) { }
+
+  ngOnInit(): void {
+  }
+
+  scrollToContact(): void {
+    const contactSection = this.elementRef.nativeElement.querySelector('#contact-section');
+    contactSection.scrollIntoView({ behavior: 'smooth', block: 'start' });
+  }
 }
